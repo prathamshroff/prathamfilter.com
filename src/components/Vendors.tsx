2a8635f38a31c164aa49a92567d88265db016931
Pratham Shroff <pshroff@bu.edu>
2023-04-30 06:58:45 -0400

    Added Vendor Carousel Component & Assets

---
+++ src/components/Vendors.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { vendors } from '../config/siteInfo/vendors';
+import SwiperCore, { Pagination, Autoplay } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+// Install Swiper modules
+SwiperCore.use([Pagination, Autoplay]);
+
+const PartnerLogos: React.FC = () => {
+  return (
+    <div className="relative bg-white py-8">
+      <div className="container mx-auto px-4">
+        <Swiper
+          slidesPerView={2}
+          spaceBetween={20}
+          autoplay={{ delay: 3000, disableOnInteraction: false }}
+          pagination={{ clickable: true, el: '.swiper-pagination' }}
+          breakpoints={{
+            // when window width is >= 640px
+            640: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+            // when window width is >= 1024px
+            1024: {
+              slidesPerView: 4,
+              spaceBetween: 30,
+            },
+          }}
+          className="swiper-container"
+        >
+          {vendors.map((vendors, index) => (
+            <SwiperSlide key={index} className="flex items-center justify-center h-full">
+              <div className="flex items-center justify-center h-full">
+                <img src={vendors.logo} alt={vendors.name} className="h-12 object-contain" />
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        <div className="swiper-pagination mt-1"></div>
+      </div>
+    </div>
+  );
+};
+
+export default PartnerLogos;
---
+++ src/config/siteInfo/vendors.ts
@@ -0,0 +1,27 @@
+export const vendors = [
+    {
+      id: 1,
+      name: 'Pentair',
+      logo: "/assets/img/logos/pentair-trans-logo.png",
+    },
+    {
+      id: 2,
+      name: 'DuPont',
+      logo: "/assets/img/logos/dupont-trans-logo.png",
+    },
+    {
+      id: 3,
+      name: 'Ion Exchange',
+      logo: "/assets/img/logos/ionexchange-trans-logo.png",
+    },
+    {
+      id: 4,
+      name: 'Shakti Pumps',
+      logo: "/assets/img/logos/shakti-trans-logo.png",
+    },
+    {
+      id: 5,
+      name: 'Tata Steel',
+      logo: "/assets/img/logos/tata-steel-trans-logo.png",
+    },
+];
